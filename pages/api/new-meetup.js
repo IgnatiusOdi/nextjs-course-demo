f1171575ce276a16c046c41d94f55481e4b9d929
Ignatius Odi <ignodi@gmail.com>
2022-07-02 11:18:38 +0700

    ready for deployment

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,25 @@
+import { MongoClient } from "mongodb";
+
+// /api/new-meetup
+// POST /api/new-meetup
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://root:wfoPPKIU81rE8x1L@cluster0.vnalqjz.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
+  }
+}
+
+export default handler;
